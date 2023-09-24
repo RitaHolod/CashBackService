92617352dbbceeb7ad059d4b2d0c5f09d7d33aad
Margarita Mingazova <zoomyzoomy@icloud.com>
2023-09-24 14:20:41 +0300

    Add tests

---
+++ src/test/java/ru/netology/TestCashBackHackService.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCashBackHackService {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void purchaseOver1000() {
+
+        int amount = 1200;
+
+        int expected = 800;
+        int actual = service.remain(1200);
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void purchaseBelow1000() {
+
+        int amount = 100;
+
+        int expected = 900;
+        int actual = service.remain(100);
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void purchase1000() {
+
+        int amount = 1000;
+
+        int expected = 1000;
+        int actual = service.remain(1000);
+
+        Assertions.assertEquals(actual, expected);
+
+    }
+}
