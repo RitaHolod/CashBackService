2c19dae4a12b51935bb70910666be56f753a059e
Margarita Mingazova <zoomyzoomy@icloud.com>
2023-09-25 13:35:07 +0300

    j5

--- src/test/java/ru/netology/TestCashBackHackService.java
+++ src/test/java/ru/netology/TestCashBackHackService.java
@@ -1,7 +1,8 @@
 package ru.netology;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 
 public class TestCashBackHackService {
     CashbackHackService service = new CashbackHackService();
